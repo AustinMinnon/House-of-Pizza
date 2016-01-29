165d294ea3d52e51ccd11740b3e9a63d73f65280
austin <student@epicodus.com>
2016-01-29 09:14:42 -0800

    add pizzaType prototype

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,6 @@ function Order(topping,size,crust,addon) {
   this.addon = addon;
   this.cart = [];
 }
+Order.prototype.pizzaType = function() {
+  return "Your " + this.crust + " crust " + this.topping + " pizza, " + this.size + " will be ready in about 15 minutes";
+}
