af9cfdc616157f418bf10124e0584a0dd6ee7b5c
AustinjMinnon <whi7ee@gmail.com>
2016-02-03 21:17:11 -0800

    add price calculator method for each property of Order, return total for Your Order display

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,59 @@ function Order(topping,size,crust,addon) {
 Order.prototype.pizzaType = function() {
   return this.crust + " crust " + this.topping + " pizza, " + this.size + ", " + this.addon ;
 }
+Order.prototype.getToppingPrice = function() {
+  var toppingPrice = 5
+
+  if (this.topping === "pepperoni" || this.topping === "xcheese" || this.topping === "bacon") {
+    return toppingPrice +=3;
+  }else {
+    return toppingPrice +0;
+  }
+  return toppingPrice;
+}
+
+Order.prototype.getSizePrice = function() {
+  var sizePrice = 0
+
+  if (this.size === "sm") {
+    return sizePrice +=1;
+  }else if (this.size === "md") {
+    return sizePrice +=3;
+  }else if (this.size === "lg") {
+    return sizePrice +=5;
+  }else {
+  }
+  return sizePrice;
+}
+
+Order.prototype.getCrustPrice = function() {
+  var CrustPrice = 0
+
+  if (this.crust === "reg") {
+    return CrustPrice +=0;
+  }else if (this.crust === "thin") {
+    return CrustPrice +=-1;
+  }else if (this.crust === "deep") {
+    return CrustPrice +=1;
+  }else {
+  }
+  return CrustPrice;
+}
+
+Order.prototype.getAddonPrice = function() {
+  var AddonPrice = 0
+
+  if (this.addon === "cola" || this.addon === "rootbeer" || this.addon === "gingerale") {
+    return AddonPrice +=2;
+  }else {
+  }
+  return AddonPrice;
+}
+
+Order.prototype.getPizzaPrice = function() {
+  return this.getToppingPrice() + this.getSizePrice() + this.getCrustPrice() + this.getAddonPrice();
+}
+
 // user interface
 $(document).ready(function() {
   $("form#new-order").submit(function(event) {
@@ -19,7 +72,7 @@ $(document).ready(function() {
     var inputtedCrust = $("input#new-crust").val();
     var inputtedAddon = $("input#new-addon").val();
     var newOrder = new Order(inputtedTopping, inputtedSize, inputtedCrust, inputtedAddon);
-
+    var total = newOrder.getPizzaPrice();
     $("ul#orders").append("<li><span class='order'>" + newOrder.pizzaType() + "</span></li>");
 
     $('input').val("");
@@ -32,6 +85,7 @@ $(document).ready(function() {
       $("p .size").text(newOrder.size);
       $("p .crust").text(newOrder.crust);
       $("p .addon").text(newOrder.addon);
+      $("p .total").text(total);
     });
   });
 });
--- spec/specs.js
+++ spec/specs.js
@@ -1,14 +1,34 @@
 describe('Order', function() {
-  it("creates a pizza order with the given properties", function() {
-    var testOrder = new Order("cheese","small","thin","liter");
+  it("creates a pizza order with properties:topping,size,crust,addon", function() {
+    var testOrder = new Order("cheese","sm","thin","liter");
     expect(testOrder.topping).to.equal("cheese");
-    expect(testOrder.size).to.equal("small");
+    expect(testOrder.size).to.equal("sm");
     expect(testOrder.crust).to.equal("thin");
     expect(testOrder.addon).to.equal("liter");
     expect(testOrder.cart).to.eql([]);
   });
-  it("adds the pizzaType method to all Orders", function() {
-    var testOrder = new Order("pepperoni","small","thin","2ltr")
-    expect(testOrder.pizzaType()).to.equal("thin crust pepperoni pizza, small, 2ltr");
+  it("return pizzaType to display in pizzas list", function() {
+    var testOrder = new Order("pepperoni","sm","thin","cola")
+    expect(testOrder.pizzaType()).to.equal("thin crust pepperoni pizza, sm, cola");
   });
+  it("calculate topping price selected", function() {
+    var testOrder = new Order("pepperoni","sm","thin","cola")
+    expect(testOrder.getToppingPrice()).to.equal(8);
+  });
+  it("calculate size price selected", function() {
+    var testOrder = new Order("pepperoni","sm","thin","cola")
+    expect(testOrder.getSizePrice()).to.equal(1);
+  })
+  it("calculate crust price selected", function() {
+    var testOrder = new Order("pepperoni","sm","thin","cola")
+    expect(testOrder.getCrustPrice()).to.equal(-1)
+  })
+  it("calculate addon soda price", function() {
+    var testOrder = new Order("pepperoni","sm","thin","cola")
+    expect(testOrder.getAddonPrice()).to.equal(2)
+  })
+  it("calculate topping plus size plus crust plus addon", function() {
+    var testOrder = new Order("pepperoni","sm","thin","cola")
+    expect(testOrder.getPizzaPrice()).to.equal(10)
+  })
 });
